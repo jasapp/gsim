5d06ba9e5295c4c827bb782a59d1c836139d0849
Jeff Sapp <jasapp@gmail.com>
2011-03-11 17:28:07 -0600

    Making it so the tests actually run, because that's important you know.

--- test/gsim/test/machine.clj
+++ test/gsim/test/machine.clj
@@ -2,17 +2,23 @@
   (:use [gsim.machine] :reload)
   (:use [clojure.test]))
 
-(testing "tokenize-block"
-  (is (tokenize-block "") [])
-  (is (tokenize-block " ") [])
-  (is (tokenize-block "  ") [])
-  (is (tokenize-block "    ") [])
-  (is (tokenize-block "g0 g0    g0    ") ["g0" "g0" "g0"]))
+(def invalid-words ["1" "a" "1a" "2a " "2 a " " 2a "])
 
-(testing "valid-word?"
-  (not (valid-word? "1"))
-  (not (valid-word? "a"))
-  (not (valid-word? "1a"))
-  (not (valid-word? "2a "))
-  (not (valid-word? "2 a "))
-  (not (valid-word? "  2a ")))
+(deftest parsing
+  (testing "tokenize-block"
+	(is (tokenize-block "") [])
+	(is (tokenize-block " ") [])
+	(is (tokenize-block "  ") [])
+	(is (tokenize-block "    ") [])
+	(is (tokenize-block "g0 g0    g0    ") ["g0" "g0" "g0"]))
+
+  (testing "valid-word?"
+	(not (valid-word? "1"))
+	(not (valid-word? "a"))
+	(not (valid-word? "1a"))
+	(not (valid-word? "2a "))
+	(not (valid-word? "2 a "))
+	(not (valid-word? "  2a ")))
+
+  (testing "code-name"
+	(is (code-name {:word "foo"}) "fooasdfasdf")))
