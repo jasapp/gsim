0da00f1bfb6d3fc6076ac6f11937917ef38d75b8
Jeff Sapp <jasapp@gmail.com>
2012-01-27 20:57:28 -0600

    Uhh, not sure what's going on here.

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject gsim "1.0.0-SNAPSHOT"
   :description "A web based gcode simulator"
   :dependencies [[org.clojure/clojure "1.2.0"]
-				 [org.clojure/clojure-contrib "1.2.0"]
-				 [compojure "0.6.4"]
-				 [hiccup "0.3.6"]
-				 [clj-json "0.3.1" ]]
+		 [org.clojure/clojure-contrib "1.2.0"]
+		 [compojure "0.6.5"]
+		 [clj-http "0.1.3"]
+		 [hiccup "0.3.6"]
+		 [clj-json "0.3.1" ]
+		 [ring/ring-jetty-adapter "0.3.11"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-					 [lein-ring "0.4.5"]
-					 [ring-serve "0.1.1"]]
-  :ring {:handler gsim.core/app})
+		     [compojure "0.6.5"]
+		     [cljs-devmode "0.1.0-SNAPSHOT"]]
+  :main gsim.core)
