4dbef877107b7c43b67545a4903b7cd468afc031
Jeff Sapp <jasapp@gmail.com>
2012-02-01 18:21:14 -0600

    Whole bunch of new stuff copying clojureone.

---
+++ src/app/clj/gsim/sample/config.clj
@@ -0,0 +1,30 @@
+(ns gsim.sample.config
+  "Contains configuration for the sample application."
+  (:require [net.cgrand.enlive-html :as html]))
+
+(defn- production-transform [h]
+  (html/transform h
+                  [:ul#navigation]
+                  (html/substitute (html/html-snippet ""))))
+
+(def ^{:doc "Configuration for the sample application."}
+  config {:src-root "src"
+          :app-root "src/app/cljs"
+          :top-level-package "gsim"
+          :js "public/javascripts"
+          :dev-js-file-name "main.js"
+          :prod-js-file-name "mainp.js"
+          :dev-js ["goog.require('one.sample.core');"
+                   "goog.require('gsim.sample.model');"
+                   "goog.require('gsim.sample.controller');"
+                   "goog.require('gsim.sample.history');"
+                   "goog.require('gsim.sample.logging');"
+                   "gsim.sample.core.start();gsim.sample.core.repl();"]
+          :prod-js ["gsim.sample.core.start();"]
+          :reload-clj ["/gsim/host_page"
+                       "/gsim/reload"
+                       "/gsim/templates"
+                       "/gsim/sample/api"
+                       "/gsim/sample/config"
+                       "/gsim/sample/dev_server"]
+          :prod-transform production-transform})
---
+++ src/leiningen/bootstrap.clj
@@ -0,0 +1,15 @@
+;;
+;; Stolen from the clojurescript one project.
+;; Thanks guys!
+;; 
+
+(ns leiningen.bootstrap
+  "Bootstrap the project by running lein deps and lein git-deps."
+  (:require leiningen.deps
+	    leiningen.git-deps))
+
+(defn bootstrap
+  "Bootstrap the project by running lein deps and lein git-deps."
+  [project]
+  (leiningen.deps/deps project)
+  (leiningen.git-deps/git-deps project))
