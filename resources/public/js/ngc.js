0831b304f49d213f54359a809e4a298d70a89fa3
Jeff Sapp <jasapp@gmail.com>
2012-07-29 18:21:13 -0500

    Messy, but again more working.

---
+++ resources/public/js/ngc.js
@@ -0,0 +1,48 @@
+/**
+ * Author: jeff sapp
+ * 
+ */
+
+CodeMirror.defineMode("ngc", function() {
+
+    var eatLimit = function(stream, re, limit) {
+	var current_count = 0;
+	var ch = stream.peek();
+
+	while (re.test(ch) && current_count < limit) {
+	    current_count = current_count + 1;
+	    stream.next();
+	}
+    };
+
+    return {
+
+	token: function(stream) {
+
+	    if (stream.eatSpace()) {
+		return null;
+	    }
+
+	    var returnType = null; 
+	    var ch = stream.next();
+
+	    if (/m/i.test(ch)) {
+		stream.eatWhile(/\d/);
+		returnType = "keyword";
+	    } else if (/g/i.test(ch)) {
+		stream.eatWhile(/\d/);
+		returnType = "tag";
+	    } else if (/[a-z]/i.test(ch)) {
+		stream.eatWhile(/(\d|\.)/);
+		returnType = "builtin";
+	    } else if (ch == '(') {
+		stream.skipToEnd();
+		returnType = "comment";
+	    } 
+
+	    return returnType;
+	}
+    };
+});
+
+CodeMirror.defineMIME("text/x-ngc", "ngc");
--- src-clj/gsim/views.clj
+++ src-clj/gsim/views.clj
@@ -16,12 +16,16 @@
    [:head
     [:title "code!"]
     [:link {:rel "stylesheet" :href "/css/codemirror.css"}]
+    [:style {:type "text/css"}
+     ".CodeMirror {border-top: 1px solid black; border-bottom: 1px solid black;}"
+     ".activeline {background: #e8f2ff !important;}"]
     (include-js "/js/codemirror.js")
-    (include-js "/js/clojure.js")
+    (include-js "/js/ngc.js")
     (include-js "/js/Three.js")]
    [:body
     [:form
-     [:textarea {:name "code" :id "code"}]]
+     [:textarea {:name "code" :id "code"}
+      "O1234\nN100\nG0 X1.0 Z1.0 Y1.0\nM3"]]
     (include-js "/js/draw-init.js")]))
 
 (defn index-page []
