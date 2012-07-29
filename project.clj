af11076050d101a54300f02f19d8dd9aa6fdc334
Jeff Sapp <jasapp@gmail.com>
2012-07-28 22:00:04 -0500

    Not sure if this is a great idea.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject gsim "1.0.0-SNAPSHOT"
   :description "A web based gcode simulator"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring "1.0.0-RC1"]
                  [compojure "0.6.4"]
                  [enlive "1.0.0"]
@@ -11,9 +11,9 @@
                      [marginalia "0.7.0-SNAPSHOT"]
                      [lein-marginalia "0.7.0-SNAPSHOT"]]
   :git-dependencies [["https://github.com/clojure/clojurescript.git"
-                      "886d8dc81812962d30a741d6d05ce9d90975160f"]
+                      "5c8e1d4ece4f660fdd5b62dc5861c8dc76b7b27c"]
                      ["https://github.com/levand/domina.git"
-                      "8933b2d12c44832c9bfaecf457a1bc5db251a774"]]
+ 		      "a328080ca4a754e808454f589caae90fac951d10"]]
   :repl-init gsim.sample.repl
   :source-path "src/app/clj"
   :extra-classpath-dirs [".lein-git-deps/clojurescript/src/clj"
