9501014c89657a30101338057df0c4eeb67691ad
Jeff Sapp <jasapp@gmail.com>
2012-08-14 22:51:48 -0500

    Fixing that nasty regex.

--- resources/public/js/ngc.js
+++ resources/public/js/ngc.js
@@ -33,7 +33,8 @@ CodeMirror.defineMode("ngc", function() {
 		stream.eatWhile(/\d/);
 		returnType = "tag";
 	    } else if (/[a-z]/i.test(ch)) {
-		stream.eatWhile(/(\+|\-|\d|\.)/);
+		stream.eat(/(\+|\-)?/);
+		stream.eatWhile(/(\d|\.)/);
 		returnType = "builtin";
 	    } else if (ch == '(') {
 		stream.skipToEnd();
